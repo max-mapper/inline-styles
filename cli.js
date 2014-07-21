d8f8941ed0bc5b36ecb1e490a325b9c1c9adf6ae
coderaiser <mnemonic.enemy@gmail.com>
2014-07-21 06:12:38 -0400

    readFileSync: toString -> utf8

--- cli.js
+++ cli.js
@@ -2,5 +2,5 @@
 
 var fs = require('fs')
 var inliner = require('./')
-var inlined = inliner(fs.readFileSync(process.argv[2])).toString()
+var inlined = inliner(fs.readFileSync(process.argv[2], 'utf8')).toString()
 console.log(inlined)
